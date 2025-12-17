ea57797b6cfe8c2ce3c4701d44defdf61e6be5d9
Prathamesh Patil <122prathamesh6002@sjcem.edu.in>
2025-12-18 00:02:15 +0530

    Enhance ChatConsumer to handle sender_id and cache
    Refactor message handling to include sender_id and cache payload.

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,34 +2,38 @@ import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+from chat.redis_helper import push_messages
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.conversation_id = self.scope["url_route"]["kwargs"]["conversation_id"]
         self.room_group_name = f"chat_{self.conversation_id}"
-
-        # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        payload = json.load(text_data)
+        message = payload.get("message")
+        sender_id = payload.get("sender_id")
+        cached_payload = {
+            "content" : message,
+            "sender_id" : sender_id,
+            "message_type": payload.get("message_type", "TEXT")
+        }
 
-        # Send message to room group
+        push_messages(self.conversation_id, cached_payload)
+
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name, {"type": "chat.message", "message": message, "sender_id" : sender_id},
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({
+            "message": message,
+            "sender_id" : event.get("sender_id"),
+        }))
